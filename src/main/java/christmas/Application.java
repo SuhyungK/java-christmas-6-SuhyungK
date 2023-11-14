4538d9e9ecadc9b803259cb1de58bb568c1b9f09
SuhyungK <dook4966@gmail.com>
2023-11-14 21:46:14 +0900

    feat(OutputView): 입력 시작 멘트와 출력 시작 멘트 출력 메서드 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        OutputView.printInputStart();
+
+        OutputView.printResultStart();
     }
 }
