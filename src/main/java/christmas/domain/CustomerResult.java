04b22a75ba121f83c4e8798140d7f56daba07309
SuhyungK <dook4966@gmail.com>
2023-11-15 21:57:09 +0900

    feat(CustomerResult): 개별 할인에 대한 저장 정보 Map으로 변경

--- src/main/java/christmas/domain/CustomerResult.java
+++ src/main/java/christmas/domain/CustomerResult.java
@@ -1,12 +1,12 @@
 package christmas.domain;
 
+import christmas.constants.DiscountInfo;
 import christmas.utils.MenuList;
 import christmas.utils.MenuType;
 import christmas.utils.Repeat;
 import christmas.view.InputView;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static christmas.domain.DateDiscount.*;
@@ -15,7 +15,7 @@ public class CustomerResult {
     public Customer customer;
     public int totalOrderAmount; // 할인 전 총주문 금액
     public int totalBenefitAmount; // 할인 금액의 합계
-    public List<Integer> discounts;
+    public Map<String, Integer> discounts;
     public MenuList giveaway = MenuList.없음;
     public EventBadge badge; // 이벤트 배지
     private Map<MenuType, Integer> menuTypeList = new HashMap<>();
@@ -25,11 +25,12 @@ public class CustomerResult {
         this.totalOrderAmount = calculateTotalAmount();
         if (this.totalOrderAmount >= EventInfo.MINIMUM_EVENT_AMOUNT.getNumber()) {
             this.giveaway = getGiveaway();
-            discounts.add(getDDayDiscount(customer.date));
-            discounts.add(calculateWeekDayDiscountAmount(getDiscountMenuType(customer.date)));
-            discounts.add(getSpecialDiscount(customer.date));
-            discounts.add(giveaway.getPrice());
-            this.totalBenefitAmount = discounts.stream().mapToInt(i -> i).sum();
+            discounts.put("크리스마스 디데이 할인", getDDayDiscount(customer.date));
+            DiscountInfo weekInfo = getWeekInfo(customer.date);
+            discounts.put(weekInfo.toString() + " 할인", calculateWeekDayDiscountAmount(weekInfo.getType()));
+            discounts.put("특별 할인", getSpecialDiscount(customer.date));
+            discounts.put("증정 이벤트", giveaway.getPrice());
+            this.totalBenefitAmount = discounts.values().stream().mapToInt(i -> i).sum();
         }
         this.badge = getBadge();
         Repeat.readMenuHandler(InputView::readOrderMenus);
