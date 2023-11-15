1466f25ec5d50c5506654187f87f9ce6e46e34c8
SuhyungK <dook4966@gmail.com>
2023-11-15 20:56:13 +0900

    refactor(EventInfo): 날짜 관련된 enum 추가

--- src/main/java/christmas/domain/EventInfo.java
+++ src/main/java/christmas/domain/EventInfo.java
@@ -2,15 +2,19 @@ package christmas.domain;
 
 public enum EventInfo {
     START_DATE(1),
-    END_DATE(31);
+    END_DATE(31),
+    DDAY_END_DATE(25),
+    DISCOUNT_PRCIE_UNIT(2_023),
+    MINIMUM_EVENT_AMOUNT(10_000),
+    MINIMUM_GIVEAWAY_EVENT_AMOUNT(120_000);
 
-    private final int date;
+    private final int number;
 
-    EventInfo(int date) {
-        this.date = date;
+    EventInfo(int number) {
+        this.number = number;
     }
 
     public int getDate() {
-        return date;
+        return number;
     }
 }
