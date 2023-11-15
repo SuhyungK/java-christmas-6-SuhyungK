3365240f6729a86e56c62e5b68297a2ea9f78d98
SuhyungK <dook4966@gmail.com>
2023-11-15 23:18:21 +0900

    feat(EventController): OutputView 객체 생성 후 결과 값 출력

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -1,18 +1,35 @@
 package christmas.controller;
 
 import christmas.domain.Customer;
+import christmas.domain.CustomerResult;
 import christmas.utils.Repeat;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 import java.util.Map;
 
 public class EventController {
     public void start() {
-//
-//        Customer customer = new Customer(date, orderMenu);
+        OutputView.printInputStart();
         int date = Repeat.readDataHandler(InputView::readDate);
         Map<String, Integer> orderMenu = Repeat.readMenuHandler(InputView::readOrderMenus);
+        OutputView.printResultStart();
+
+        Customer customer = Customer.of(date, orderMenu);
+        CustomerResult result = CustomerResult.of(customer);
+
+        outputHandler(result);
+    }
 
 
+    private void outputHandler(CustomerResult result) {
+        OutputView outputView = new OutputView(result);
+        outputView.printMenu();
+        outputView.printTotalOrderAmount();
+        outputView.printGiveaway();
+        outputView.printBenefit();
+        outputView.printTotalBenefitAmount();
+        outputView.printPaymentAmount();
+        outputView.printEventBadge();
     }
 }
