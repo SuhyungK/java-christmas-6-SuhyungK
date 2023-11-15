b4aea7df8f8ad846849150b742cfccb3e7d8f38c
SuhyungK <dook4966@gmail.com>
2023-11-15 17:56:06 +0900

    feat(MenuList): 메뉴에 대한 분류와 가격 정보 enum 선언

---
+++ src/main/java/christmas/utils/MenuList.java
@@ -0,0 +1,31 @@
+package christmas.utils;
+
+public enum MenuList {
+    양송이수프("에피타이저", 6_000),
+    타파스("에피타이저", 5_500),
+    시저샐러드("에피타이저", 8_000),
+    티본스테이크("메인", 55_000),
+    바비큐립("메인", 54_000),
+    해산물파스타("메인", 35_000),
+    크리스마스파스타("메인", 25_000),
+    초코케이크("디저트", 15_000),
+    아이스크립("디저트", 5_000),
+    제로콜라("음료", 3_000),
+    레드와인("음료", 60_000),
+    샴페인("음료", 25_000);
+
+    private final String type;
+    private final int price;
+    MenuList(String type, int price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
