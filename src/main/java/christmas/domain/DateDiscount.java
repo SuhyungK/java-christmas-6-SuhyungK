721ed57fb63787d98c68a4b8048c1edec8349f62
SuhyungK <dook4966@gmail.com>
2023-11-15 20:03:28 +0900

    refactor(DateDiscount): 특별 할인 반환 값 boolean에서 int로 변경

--- src/main/java/christmas/domain/DateDiscount.java
+++ src/main/java/christmas/domain/DateDiscount.java
@@ -2,7 +2,6 @@ package christmas.domain;
 
 import christmas.utils.MenuType;
 
-import java.awt.*;
 import java.time.LocalDate;
 
 public class DateDiscount {
@@ -11,6 +10,7 @@ public class DateDiscount {
     private static final int YEAR = 2023;
     private static final int MONTH = 12;
     private static final int SPECIAL_DATE = 25;
+    private static final int SPECIAL_UNIT_AMOUNT = 1_000;
 
 
     public static int getDDayDiscount(int date) {
@@ -20,22 +20,26 @@ public class DateDiscount {
         return DDAY_START_AMOUNT + --date * DDAY_UNIT_AMOUNT;
 
     }
-    public static MenuType getDiscountMenuType(int weekNumber) {
+
+    public static MenuType getDiscountMenuType(int date) {
+        int weekNumber = getWeekNumber(date);
         if (weekNumber == 5 || weekNumber == 6) {
             return MenuType.메인;
         }
         return MenuType.디저트;
     }
 
-    public static boolean isSpecialDate(int date) {
+    public static int getSpecialDiscount(int date) {
         if (getWeekNumber(date) == 6 || date == SPECIAL_DATE) {
-            return true;
+            return SPECIAL_UNIT_AMOUNT;
         }
-        return false;
+        return 0;
     }
 
     private static int getWeekNumber(int date) {
-        return LocalDate.of(YEAR, MONTH, date).getDayOfWeek().getValue();
+        return LocalDate.of(YEAR, MONTH, date)
+                        .getDayOfWeek()
+                        .getValue();
     }
 
 }
