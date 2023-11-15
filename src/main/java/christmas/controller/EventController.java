bf3893abd4edd31fbc6c6898506c8095229784dd
SuhyungK <dook4966@gmail.com>
2023-11-15 15:21:54 +0900

    feat(Repeat): 입력값에 대해 유효성 검사 후 유효하지 않은 경우 다시 입력 받는 메서드 구현

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -1,7 +1,18 @@
 package christmas.controller;
 
+import christmas.domain.Customer;
+import christmas.utils.Repeat;
+import christmas.view.InputView;
+
+import java.util.Map;
+
 public class EventController {
     public void start() {
+//
+//        Customer customer = new Customer(date, orderMenu);
+        int date = Repeat.readDataHandler(InputView::readDate);
+        Map<String, Integer> orderMenu = Repeat.readMenuHandler(InputView::readOrderMenus);
+
 
     }
 }
---
+++ src/main/java/christmas/utils/Repeat.java
@@ -0,0 +1,40 @@
+package christmas.utils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class Repeat {
+    public static int readDataHandler(Supplier<String> supplier) {
+        try {
+            String data = supplier.get();
+            Validator.isNumber(data);
+            Validator.isValidDate(Integer.parseInt(data));
+            return Integer.parseInt(data);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ErrorMessage.날짜);
+            return readDataHandler(supplier);
+        }
+    }
+
+    public static Map<String, Integer> readMenuHandler(Supplier<String> supplier) {
+        try {
+            String data = supplier.get();
+            return Arrays.stream(data.split(","))
+                         .map(each -> {
+                             String[] order = each.split("-");
+                             Validator.isValidForm(order);
+                             Validator.isMenuExist(order[0]);
+                             Validator.isNumber(order[1]);
+                             return Map.entry(order[0], Integer.parseInt(order[1]));
+                         })
+                         .collect(HashMap::new,
+                                 (map, entry) ->
+                                         map.put(entry.getKey(), entry.getValue()), HashMap::putAll);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ErrorMessage.주문);
+            return readMenuHandler(supplier);
+        }
+    }
+}
